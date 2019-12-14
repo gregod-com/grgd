ca8ad3ebd0a835788a6e5ca3e472508e009eebad
Gregor Pirolt <gregor.pirolt@me.com>
2019-12-15 00:19:54 +0100

    move tests into test folder

--- templates/help.go
+++ templates/help.go
@@ -8,10 +8,10 @@ func GetHelpTemplate() string {
 	{{if .UsageText}}{{.UsageText}}{{else}}{{.HelpName}} {{if .VisibleFlags}}[global options]{{end}}{{if .Commands}} command [command options]{{end}} {{if .ArgsUsage}}{{.ArgsUsage}}{{else}}[arguments...]{{end}}{{end}}
 	{{if .VisibleCommands}}
  COMMANDS:{{range .VisibleCategories}}{{if .Name}}
- 
+
 	{{.Name}}:{{end}}{{range .VisibleCommands}}
 	  {{join .Names ", "}}{{"\t\t\t-> "}}{{.Usage}}{{end}}{{end}}{{end}}{{if .VisibleFlags}}
- 
+
  GLOBAL OPTIONS:
 	{{range $index, $option := .VisibleFlags}}{{if $index}}
 	{{end}}{{$option}}{{end}}{{end}}
