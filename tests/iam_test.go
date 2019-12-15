e14d53d5fdb194f700e5578f710c2099aa34334c
Gregor Pirolt <gregor.pirolt@me.com>
2019-12-15 01:14:45 +0100

    move tests

---
+++ tests/iam_test.go
@@ -0,0 +1,41 @@
+package test
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"log"
+	"bytes"
+)
+
+// capture output of a function and return the string
+func captureOutput(f func()) string {
+    var buf bytes.Buffer
+    log.SetOutput(&buf)
+    f()
+    log.SetOutput(os.Stderr)
+    return buf.String()
+}
+
+func TestHelloWorld(t *testing.T) {
+	WelloHorld := "Wello Horld"
+	if WelloHorld != "Wello Horld" {
+		t.Errorf("wrong")
+	}
+}
+
+func TestPrintBanner(t *testing.T) {
+	PrintBanner()
+}
+
+func TestMainFunction(t *testing.T) {
+	// GIVEN
+	output := captureOutput(func() {PrintBanner()})
+
+	// WHEN
+	want := "iamCLI"
+
+	// THEN
+	assert.Containsf(t, output, want, "Does not contain %s", want)
+
+}
