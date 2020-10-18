de13bdf4eeaa9bf86eb2f4df9b6225fe7c0eba69
Gregor Pirolt <gregor.pirolt@me.com>
2020-10-18 20:57:29 +0200

    remove unused flags

--- clicommands/flags/flags.go
+++ clicommands/flags/flags.go
@@ -7,21 +7,6 @@ import (
 // GetFlags ...
 func GetFlags() []cli.Flag {
 	return []cli.Flag{
-		&cli.BoolFlag{
-			Name:    "debug",
-			Aliases: []string{"d"},
-			Usage:   "run the command in debug mode",
-		},
-		&cli.BoolFlag{
-			Name:    "verbose",
-			Aliases: []string{"vvv"},
-			Usage:   "run the command in verbose mode",
-		},
-		&cli.BoolFlag{
-			Name:    "silent",
-			Aliases: []string{"s"},
-			Usage:   "mute all outputs",
-		},
 		&cli.StringFlag{
 			Name:    "profile",
 			EnvVars: []string{"USER"},
