a6a78be7aebf6407fdef766011f3a5bd99e6733f
Gregor Pirolt <gregor.pirolt@me.com>
2020-10-18 20:59:20 +0200

    add generation of mocks from grgdplugincontracts to script

--- interfaces/makeMocks.sh
+++ interfaces/makeMocks.sh
@@ -4,3 +4,9 @@ for file in *.go; do
 echo $file
 mockgen --source=$file -destination mocks/mock$file -package mocks
 done
+
+for file in ../../grgdplugincontracts/*.go; do
+echo $file
+mockgen --source=$file -destination mocks/mock$(basename $file) -package mocks
+done
+
