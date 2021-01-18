4f2ef93355f9d33384d9211f1583005d9090c470
Gregor Pirolt <gregor.pirolt@iamstudent.at>
2021-01-18 15:46:34 +0100

    add native stack commands and actions

---
+++ clicommands/stack/stackActions.go
@@ -0,0 +1,40 @@
+package stack
+
+import (
+	"github.com/urfave/cli/v2"
+)
+
+// AUp start the stack
+func AUp(c *cli.Context) error {
+	// shared.Debug(c, "called")
+	// ----------------------
+	return nil
+}
+
+// ADown ...
+func ADown(c *cli.Context) error {
+	// shared.Debug(c, "called")
+	// ----------------------
+	return nil
+}
+
+// ARestart ...
+func ARestart(c *cli.Context) error {
+	// shared.Debug(c, "called")
+	// ----------------------
+	return nil
+}
+
+// ALogs ...
+func ALogs(c *cli.Context) error {
+	// shared.Debug(c, "called")
+	// ----------------------
+	return nil
+}
+
+// AActivate ...
+func AActivate(c *cli.Context) error {
+	// shared.Debug(c, "called")
+	// ----------------------
+	return nil
+}
---
+++ clicommands/stack/stackCommands.go
@@ -0,0 +1,83 @@
+package stack
+
+import (
+	"github.com/urfave/cli/v2"
+)
+
+// GetCLICommands ...
+func GetCLICommands(app *cli.App) []*cli.Command {
+	return []*cli.Command{
+		{
+			Name:        "up",
+			Category:    "local stack",
+			Usage:       "Start dev stack with current config",
+			Aliases:     []string{"u"},
+			Action:      AUp,
+			Description: "up description",
+		},
+		{
+			Name:        "down",
+			Category:    "local stack",
+			Usage:       "Stop dev stack",
+			Aliases:     []string{"d"},
+			Action:      ADown,
+			Description: "down description",
+		},
+		{
+			Name:        "restart",
+			Category:    "stack",
+			Usage:       "Restart dev stack (or single workload)",
+			Aliases:     []string{"r"},
+			Action:      ARestart,
+			Description: "restart description",
+		},
+		{
+			Name:        "logs",
+			Category:    "stack",
+			Usage:       "Show all logs for running stack (or single workload)",
+			Aliases:     []string{"l"},
+			ArgsUsage:   "Args usage",
+			Action:      ALogs,
+			Description: "logs description",
+		},
+		{
+			Name:      "activate",
+			Category:  "stack",
+			Usage:     "Activate a workload",
+			Aliases:   []string{"act", "a"},
+			ArgsUsage: "TODO",
+			Action:    AActivate,
+			After: func(c *cli.Context) error {
+				// UIPlugin.PrintWorkloadOverview(c)
+				return nil
+			},
+			Description: "activate description",
+		},
+		{
+			Name:        "enter",
+			Category:    "stack",
+			Usage:       "Enter a workload",
+			Aliases:     []string{"en"},
+			Action:      AActivate,
+			Description: "enter description",
+		},
+		{
+			Name:        "execute",
+			Category:    "stack",
+			Usage:       "Execute a command in workload and view output",
+			Aliases:     []string{"exec", "ex"},
+			Action:      AActivate,
+			Description: "execute description",
+		},
+		{
+			Name:     "test",
+			Category: "stack",
+			Usage:    "Run unittest inside container",
+			Aliases:  []string{"t"},
+			// Flags:       myFlags,
+			ArgsUsage:   "TODO",
+			Action:      AActivate,
+			Description: "test description",
+		},
+	}
+}
