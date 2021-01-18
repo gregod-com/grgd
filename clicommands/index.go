fe2a0ed4703cc6cc51b72089d9164823100f9560
Gregor Pirolt <gregor.pirolt@iamstudent.at>
2021-01-18 15:39:47 +0100

    remove unfinished commands from loading

--- clicommands/index.go
+++ clicommands/index.go
@@ -1,12 +1,11 @@
 package clicommands
 
 import (
-	"grgd/clicommands/config"
-	"grgd/clicommands/plugin"
-	"grgd/clicommands/profile"
-	"grgd/clicommands/project"
-	"grgd/clicommands/service"
-	"grgd/clicommands/shortcut"
+	"github.com/gregod-com/grgd/clicommands/aws"
+	"github.com/gregod-com/grgd/clicommands/config"
+	"github.com/gregod-com/grgd/clicommands/hack"
+	"github.com/gregod-com/grgd/clicommands/k8s"
+	"github.com/gregod-com/grgd/clicommands/update"
 
 	"github.com/urfave/cli/v2"
 )
@@ -15,12 +14,20 @@ import (
 func GetCommands(app *cli.App) []*cli.Command {
 	var myCommands []*cli.Command
 
-	myCommands = append(myCommands, project.GetCLICommands(app)...)
-	myCommands = append(myCommands, service.GetCLICommands(app)...)
-	myCommands = append(myCommands, profile.GetCLICommands(app)...)
-	myCommands = append(myCommands, plugin.GetCLICommands(app)...)
+	myCommands = append(myCommands, aws.GetCLICommands(app)...)
+	myCommands = append(myCommands, k8s.GetCLICommands(app)...)
 	myCommands = append(myCommands, config.GetCLICommands(app)...)
-	myCommands = append(myCommands, shortcut.GetCLICommands(app)...)
+	// myCommands = append(myCommands, initialise.GetCLICommands(app)...)
+	// myCommands = append(myCommands, shortcut.GetCLICommands(app)...)
+	myCommands = append(myCommands, update.GetCLICommands(app)...)
+	myCommands = append(myCommands, hack.GetCLICommands(app)...)
+
+	// myCommands = append(myCommands, project.GetCLICommands(app)...)
+	// myCommands = append(myCommands, service.GetCLICommands(app)...)
+	// myCommands = append(myCommands, profile.GetCLICommands(app)...)
+	// myCommands = append(myCommands, plugin.GetCLICommands(app)...)
+	// myCommands = append(myCommands, dns.GetCLICommands(app)...)
+	// myCommands = append(myCommands, stack.GetCLICommands(app)...)
 
 	return myCommands
 }
