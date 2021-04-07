33c6a79fda12a36b4bbad2a9364d32959738a98c
Gregor Pirolt <gregor.pirolt@iamstudent.at>
2021-04-07 14:23:55 +0200

    remove pluginloader interface

--- interfaces/IPluginLoader.go
+++
@@ -1,7 +0,0 @@
-package interfaces
-
-// IPluginLoader ...
-type IPluginLoader interface {
-	LoadPlugins(pluginFolder string) ([]ICMDPlugin, IUIPlugin)
-	// LoadHack(scriptsFolder string) []grgdplugincontracts.ICMDPlugin
-}
