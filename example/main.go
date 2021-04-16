be6d97bdc05789cf59ce486191e8566a77465753
Gregor Pirolt <gregor.pirolt@iamstudent.at>
2021-04-16 14:38:52 +0200

    add basic example for wrapper application

--- example/main.go
+++ example/main.go
@@ -2,14 +2,9 @@
 package main
 
 import (
-	"log"
-	"os"
-	"sort"
-	"time"
-
-	"github.com/gregod-com/grgd/cmd"
-	"github.com/gregod-com/grgd/cmd/flags"
+	"github.com/gregod-com/grgd"
 	"github.com/gregod-com/grgd/core"
+	"github.com/gregod-com/grgd/example/group1"
 	"github.com/gregod-com/grgd/interfaces"
 	"github.com/gregod-com/grgd/pkg/config"
 	"github.com/gregod-com/grgd/pkg/gormdal"
@@ -17,88 +12,25 @@ import (
 	"github.com/gregod-com/grgd/pkg/logger"
 	"github.com/gregod-com/grgd/pkg/profile"
 	"github.com/gregod-com/grgd/view"
-
-	"github.com/urfave/cli/v2"
 )
 
 func main() {
 	dependecies := map[string]interface{}{
-		"IHelper":    helper.ProvideHelper,
-		"IUIPlugin":  view.ProvideFallbackUI,
-		"ILogger":    logger.ProvideLogrusLogger,
-		"INetworker": helper.ProvideNetworker,
-		"IDAL":       gormdal.ProvideDAL,
-		"IConfig":    config.ProvideConfig,
-		"IProfile":   profile.ProvideProfile,
+		"ILogger":     logger.ProvideLogrusLogger,
+		"IConfig":     config.ProvideConfig,
+		"IHelper":     helper.ProvideHelper,
+		"INetworker":  helper.ProvideNetworker,
+		"IDAL":        gormdal.ProvideDAL,
+		"IProfile":    profile.ProvideProfile,
+		"IUIPlugin":   view.ProvideFallbackUI,
+		"my-commands": ProvideCommands,
 	}
-
 	core := core.RegisterDependecies(dependecies)
-	logger := core.GetLogger()
+	grgd.NewApp(core, "example", "0.0.1")
+}
 
-	app := cli.NewApp()
-	app.Name = "grgd"
-	app.Usage = "grgd cli"
-	app.Version = "0.14.1"
-	app.Metadata = make(map[string]interface{})
-	app.Metadata["core"] = core
-	app.Flags = append(app.Flags, flags.GetFlags()...)
-	app.CustomAppHelpTemplate = view.GetHelpTemplate()
-	app.HideHelpCommand = true
-
-	// define behavior before every command execution
-	app.Before = func(c *cli.Context) error {
-		core := helper.GetExtractor().GetCore(c)
-		UI := core.GetUI()
-		var networker interfaces.INetworker
-		helper.GetExtractor().GetCore(c).Get(&networker)
-
-		connections := map[string]interface{}{
-			"first": &helper.Connection{
-				Endpoint: "https://www.google.com",
-				TimeOut:  100,
-				Success:  false,
-			},
-		}
-
-		networker.CheckConnections(connections)
-		for _, v := range connections {
-			logger.Info(v)
-		}
-
-		UI.ClearScreen(c)
-		UI.PrintBanner(c)
-		return nil
-	}
-
-	// define native commands available also without plugins
-	app.Commands = append(app.Commands, cmd.GetCommands(app, core)...)
-
-	// append native commands with commands found in loaded plugins
-	for _, plug := range core.GetCMDPlugins() {
-		switch arr := plug.GetCommands(nil).(type) {
-		case []*cli.Command:
-			app.Commands = append(app.Commands, arr...)
-		default:
-			logger.Errorf("Commands are not implemented as []*cli.Command but %T", arr)
-		}
-	}
-
-	// define behavior after every command execution
-	app.After = func(c *cli.Context) error {
-		core := helper.GetExtractor().GetCore(c)
-		cnfg := core.GetConfig()
-		err := cnfg.Save()
-		if err != nil {
-			logger.Fatalf("So this has failes %v", err)
-		}
-		logger.Trace(time.Since(core.GetStartTime()))
-		return nil
-	}
-
-	sort.Sort(cli.FlagsByName(app.Flags))
-
-	apperr := app.Run(os.Args)
-	if apperr != nil {
-		log.Fatal(apperr)
+func ProvideCommands() []interfaces.ICMDPlugin {
+	return []interfaces.ICMDPlugin{
+		&group1.CMD{},
 	}
 }
