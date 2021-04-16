c1888dd8e0237810922a98edf0c86de7b34c82a1
Gregor Pirolt <gregor.pirolt@iamstudent.at>
2021-04-16 14:35:07 +0200

    add back in profile, project and service commands

--- cmd/index.go
+++ cmd/index.go
@@ -13,10 +13,10 @@ func GetCommands(app *cli.App, core interfaces.ICore) []*cli.Command {
 	// myCommands = append(myCommands, config.GetCLICommands(app, core)...)
 	// myCommands = append(myCommands, hack.GetCLICommands(app, core)...)
 	myCommands = append(myCommands, update.GetCLICommands(app, core)...)
-	// myCommands = append(myCommands, profile.GetCLICommands(app, core)...)
-	// myCommands = append(myCommands, project.GetCLICommands(app, core)...)
 	// myCommands = append(myCommands, serve.GetCLICommands(app, core)...)
-	// myCommands = append(myCommands, service.GetCLICommands(app, core)...)
+	myCommands = append(myCommands, profile.GetCLICommands(app, core)...)
+	myCommands = append(myCommands, project.GetCLICommands(app, core)...)
+	myCommands = append(myCommands, service.GetCLICommands(app, core)...)
 	// myCommands = append(myCommands, stack.GetCLICommands(app, core)...)
 	// myCommands = append(myCommands, initialise.GetCLICommands(app)...)
 
