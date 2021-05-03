d7145c17a0597ef2b2e35f1d656074dc88108f28
Gregor Pirolt <gregor.pirolt@iamstudent.at>
2021-05-03 09:43:22 +0200

    load hacks as commands

--- cmd/hack/hackActions.go
+++ cmd/hack/hackActions.go
@@ -1,36 +1,19 @@
 package hack
 
 import (
-	"bytes"
-	"io"
-	"os"
-	"os/exec"
 	"path"
 
 	"github.com/gregod-com/grgd/pkg/helper"
 	"github.com/urfave/cli/v2"
 )
 
-func catchOutput(script string, silent bool, args ...string) (string, error) {
-	cmd := exec.Command(script, args...)
-	var out, errout bytes.Buffer
-	cmd.Stdout = io.MultiWriter(os.Stdout, &out)
-	cmd.Stderr = io.MultiWriter(os.Stderr, &errout)
-	if silent {
-		cmd.Stdout = &out
-		cmd.Stderr = &errout
-	}
-	err := cmd.Run()
-	return out.String() + errout.String(), err
-}
-
 // AExec ...
 func AExec(c *cli.Context) error {
 	core := helper.GetExtractor().GetCore(c)
 	// UI := core.GetUI()
+	h := core.GetHelper()
 	log := core.GetLogger()
-
-	script := path.Join(core.GetConfig().GetActiveProfile().GetBasePath(), "hack", c.Command.FullName())
+	script := path.Join(core.GetConfig().GetActiveProfile().GetMetaData("hackDir"), c.Command.FullName())
 
 	args := c.Args().Slice()
 	prefix := "exec"
@@ -38,30 +21,11 @@ func AExec(c *cli.Context) error {
 		prefix = args[0]
 	}
 
-	// profile := core.GetConfig().GetActiveProfile()
-	// bts, err := proto.Marshal(&profile)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
 	args = append([]string{prefix}, args...)
-
-	// if !strings.Contains(c.Command.Description, "shell-script") {
-	// 	args = append([]string{"--bin", base64.StdEncoding.EncodeToString(bts)}, args...)
-	// }
-
 	log.Debug("Calling plugin with arguments: %v\n", args)
 
-	if _, err2 := catchOutput(script, false, args...); err2 != nil {
+	if _, err2 := h.CatchOutput(script, false, args...); err2 != nil {
 		return err2
 	}
 	return nil
 }
-
-// func checkLsExists() {
-// 	path, err := exec.LookPath("ls")
-// 	if err != nil {
-// 		fmt.Printf("didn't find 'ls' executable\n")
-// 	} else {
-// 		fmt.Printf("'ls' executable is in '%s'\n", path)
-// 	}
-// }
--- cmd/index.go
+++ cmd/index.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"github.com/gregod-com/grgd/cmd/hack"
 	"github.com/gregod-com/grgd/cmd/profile"
 	"github.com/gregod-com/grgd/cmd/project"
 	"github.com/gregod-com/grgd/cmd/service"
@@ -14,7 +15,7 @@ import (
 func GetCommands(app *cli.App, core interfaces.ICore) []*cli.Command {
 	var myCommands []*cli.Command
 	// myCommands = append(myCommands, config.GetCLICommands(app, core)...)
-	// myCommands = append(myCommands, hack.GetCLICommands(app, core)...)
+	myCommands = append(myCommands, hack.GetCLICommands(app, core)...)
 	myCommands = append(myCommands, update.GetCLICommands(app, core)...)
 	myCommands = append(myCommands, profile.GetCLICommands(app, core)...)
 	myCommands = append(myCommands, project.GetCLICommands(app, core)...)
