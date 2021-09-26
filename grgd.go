cce128a1a5c9a1f48490072c337b7066603f61e2
Gregor Pirolt <gregor.pirolt@iamstudent.at>
2021-09-26 13:06:00 +0200

    ## features - enable hooks

--- grgd.go
+++ grgd.go
@@ -1,4 +1,3 @@
-// This package implements the cli for the iam-stack. The underlying framework is depnedent upon urfave/cli.
 package grgd
 
 import (
@@ -9,11 +8,12 @@ import (
 	"github.com/gregod-com/grgd/cmd/flags"
 	"github.com/gregod-com/grgd/interfaces"
 	"github.com/gregod-com/grgd/pkg/helper"
+	"github.com/gregod-com/grgd/view"
 
 	"github.com/urfave/cli/v2"
 )
 
-func NewApp(core interfaces.ICore, name string, version string) {
+func NewApp(core interfaces.ICore, name string, version string, hooks map[string]func(*cli.Context) error) {
 	logger := core.GetLogger()
 
 	app := cli.NewApp()
@@ -22,19 +22,27 @@ func NewApp(core interfaces.ICore, name string, version string) {
 	app.Version = version
 	app.Metadata = make(map[string]interface{})
 	app.Metadata["core"] = core
+	for k, v := range hooks {
+		app.Metadata[k] = v
+	}
 	app.Flags = append(app.Flags, flags.GetFlags()...)
-	// app.CustomAppHelpTemplate = view.GetHelpTemplate()
+	app.CustomAppHelpTemplate = view.GetHelpTemplate()
 	app.HideHelpCommand = true
 	app.HideHelp = true
-	app.HideVersion = true
+	app.UsageText = `iam [global options] command [command options] [arguments...]`
 
 	// define behavior before every command execution
 	app.Before = func(c *cli.Context) error {
 		core := helper.GetExtractor().GetCore(c)
 		UI := core.GetUI()
 		core.GetConfig().SetActiveProfile(c.String("profile"))
+		log := core.GetLogger()
+		if err := core.GetUpdater().CheckSinceLastUpdate(app.Version, core); err != nil {
+			log.Warnf("Error checking for update: %s ", err.Error())
+		}
 		UI.ClearScreen(c)
 		UI.PrintBanner(c)
+
 		return nil
 	}
 
@@ -56,6 +64,7 @@ func NewApp(core interfaces.ICore, name string, version string) {
 		core := helper.GetExtractor().GetCore(c)
 		cnfg := core.GetConfig()
 		err := cnfg.Save()
+
 		if err != nil {
 			logger.Fatalf("So this has failes %v", err)
 		}
